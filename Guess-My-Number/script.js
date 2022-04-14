76de8baffb1cf6308665ee539131abecffc7761b
faraz vaissi <farazmrx@gmail.com>
2022-04-14 11:34:23 +0430

    Refactoring and cleanup my code

--- Guess-My-Number/script.js
+++ Guess-My-Number/script.js
@@ -4,14 +4,18 @@ let secretNumber = Math.trunc(Math.random() * 20) + 1;
 let score = 20;
 let highScore = 0;
 
+const displayMessage = function(message) {
+    document.querySelector('.message').textContent = message;
+}
+
 
 document.querySelector('.check').addEventListener('click', function() {
     const guess = Number(document.querySelector('.guess').value);
     
     if (!guess) {
-        document.querySelector('.message').textContent = 'No Number!';
+        displayMessage('No Number!');
     } else if (guess === secretNumber) {
-        document.querySelector('.message').textContent = 'Correct Number!';
+        displayMessage('Correct Number!');
         document.querySelector('body').style.backgroundColor = '#60b347';
         document.querySelector('.number').style.width = '30rem';
         document.querySelector('.number').textContent = secretNumber;
@@ -20,26 +24,17 @@ document.querySelector('.check').addEventListener('click', function() {
             highScore = score;
             document.querySelector('.highscore').textContent = score;
         }
-    } else if (guess > secretNumber && guess <= 20) {
+    } else if (guess !== secretNumber) {
         if (score > 1) {
-            document.querySelector('.message').textContent = 'Too high';
+            displayMessage(guess > secretNumber ? 'Too high!' : 'Too low!');
             document.querySelector('.score').textContent = --score;
         } else {
-            document.querySelector('.message').textContent = 'You lose!';
-            document.querySelector('.score').textContent = 0;
-            document.querySelector('body').style.backgroundColor = 'red';
-        }
-    } else if (guess < secretNumber && guess > 0) {
-        if (score > 1) {
-            document.querySelector('.message').textContent = 'Too low';
-            document.querySelector('.score').textContent = --score;
-        } else {
-            document.querySelector('.message').textContent = 'You lose!';
+            displayMessage('You lose!');
             document.querySelector('.score').textContent = 0;
             document.querySelector('body').style.backgroundColor = 'red';
         }
     } else {
-        document.querySelector('.message').textContent = 'Please Enter Number Between 1-20';
+        displayMessage('Please Enter Number Between 1-20');
     }
 });
 
@@ -47,7 +42,7 @@ document.querySelector('.again').addEventListener('click', function() {
     secretNumber = Math.trunc(Math.random() * 20) + 1;
     score = 20;
 
-    document.querySelector('.message').textContent = 'start guessing...';
+    displayMessage('start guessing...');
     document.querySelector('.score').textContent = score;
     document.querySelector('.number').textContent = '?';
     document.querySelector('.guess').value = "";
